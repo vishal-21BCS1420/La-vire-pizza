4b8f3dae6aeb674b38d28d137cae6288bed84f87
Vishal Singh <svishal@telaverge.com>
2025-01-28 20:16:59 +0530

    new changes

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-center text-gray-300 py-8 px-4">
       <p className="text-sm">
-          &copy; 2024 PatelEats. All rights reserved.
+          &copy; 2025 La vire pizza. All rights reserved.
         </p> 
     </footer>
   );
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -50,7 +50,7 @@ const Navbar = () => {
     <div className="max-w-7xl mx-auto">
       <div className="flex items-center justify-between h-14">
         <Link to="/">
-          <h1 className="font-bold md:font-extrabold text-2xl">PatelEats</h1>
+          <h1 className="font-bold md:font-extrabold text-2xl">La vire pizza</h1>
         </Link>
         <div className="hidden md:flex items-center gap-10">
           <div className="hidden md:flex items-center gap-6">
